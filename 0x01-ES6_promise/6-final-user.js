71158d54b5a60924a29a89274121f996b740637d
Badre Ziani <badreziani@gmail.com>
2024-10-02 14:32:53 +0000

    Create task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,10 +2,9 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise
-    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((responses) => (
-      respones.map((res) => ({
+      responses.map((res) => ({
         status: res.status,
         value: res.status === 'fulfilled' ? res.value : String(res.reason),
       }))
